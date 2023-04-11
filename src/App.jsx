e081e1e95bd652253c5df07b7b3e9bcde84726c2
mrtripop <tripop.tor@qualint.io>
2023-04-12 00:52:01 +0700

    initial base component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { TooltipComponent } from '@syncfusion/ej2-react-popups'
-import { FiSettings } from 'react-icons/fi'
-
-import './App.css'
+import { ColorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import Topbar from './pages/Layout/Topbar'
 
 const App = () => {
+  const [theme, colorMode] = useMode()
   return (
-    <div>
-      <BrowserRouter>
-        <div className='relative flex dark:bg-main--dark-bg'>
-          <div className='fixed right-4 bottom-4' style={{ zIndex: '1000 ' }}>
-            <TooltipComponent content={'Settings'} position='top'>
-              <button>
-                <FiSettings />
-              </button>
-            </TooltipComponent>
-          </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className='app'>
+          <main className='content'>
+            <Topbar />
+          </main>
         </div>
-      </BrowserRouter>
-    </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
