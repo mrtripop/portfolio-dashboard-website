c5c6b3f21e50f54638be12fea04e8dd97cc9f570
mrtripop <tripop.tor@qualint.io>
2023-04-12 00:51:28 +0700

    initial main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.jsx'
+
 import './main.css'
 
 const domNode = document.getElementById('root')
 const root = createRoot(domNode)
 
-root.render(<App />)
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+)
