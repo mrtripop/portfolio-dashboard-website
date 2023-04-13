8d12a6d415360953e26fc711ac0c52fd144e396a
mrtripop <tripop.tor@qualint.io>
2023-04-14 00:58:53 +0700

    add other page and update route of each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import { ColorModeContext, useMode } from './theme'
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import Topbar from './pages/Layout/Topbar'
+import { Route, Routes } from 'react-router-dom'
+import Topbar from './Layout/Topbar'
+import Sidebar from './Layout/Sidebar'
+import Dashboard from './pages/dashboard'
+import Team from './pages/team'
+import Contacts from './pages/contacts'
+import Invoices from './pages/invoices'
+import Form from './pages/form'
+import Bar from './pages/bar'
+import Line from './pages/line'
+import Pie from './pages/pie'
+import FAQ from './pages/faq'
+import Geography from './pages/geography'
+import Calendar from './pages/calendar'
 
 const App = () => {
   const [theme, colorMode] = useMode()
@@ -9,8 +22,22 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className='app'>
+          <Sidebar />
           <main className='content'>
             <Topbar />
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/team' element={<Team />} />
+              <Route path='/contacts' element={<Contacts />} />
+              <Route path='/invoices' element={<Invoices />} />
+              <Route path='/form' element={<Form />} />
+              <Route path='/bar' element={<Bar />} />
+              <Route path='/line' element={<Line />} />
+              <Route path='/pie' element={<Pie />} />
+              <Route path='/faq' element={<FAQ />} />
+              <Route path='/geography' element={<Geography />} />
+              <Route path='/calendar' element={<Calendar />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
