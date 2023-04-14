2672880bee328e01b463f6236a544001a39c62de
mrtripop <tripop.tor@qualint.io>
2023-04-14 17:16:54 +0700

    add geography chart and geography page

--- src/pages/geography/index.jsx
+++ src/pages/geography/index.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { Box } from '@mui/material'
+import Header from '../../components/Header'
+import GeographyChart from '../../components/GeographyChart'
+import { useTheme } from '@emotion/react'
+import { tokens } from '../../theme'
 
 const Geography = () => {
-  return <div>Geography</div>
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+  return (
+    <Box m='20px'>
+      <Header title='Geography Chart' subtitle='Simple Geography Chart' />
+      <Box
+        sx={{ height: '75vh' }}
+        border={`1px solid ${colors.gray[100]}`}
+        borderRadius='4px'
+      >
+        <GeographyChart />
+      </Box>
+    </Box>
+  )
 }
 
 export default Geography
